f28e0c6e6cbd9a7255865d17e54671f7b2b7c8de
plozovik <plozovikov@gmail.com>
2023-04-29 19:43:18 +0200

    JsApp: added collector integration

--- JsApp/instrumentation.js
+++ JsApp/instrumentation.js
@@ -1,18 +1,29 @@
-const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { PeriodicExportingMetricReader, ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+const opentelemetry = require("@opentelemetry/sdk-node");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-proto");
+const {
+  OTLPMetricExporter
+} = require("@opentelemetry/exporter-metrics-otlp-proto");
 
-// For troubleshooting, set the log level to DiagLogLevel.DEBUG
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
-
-const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter()
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    // url: "<your-otlp-endpoint>/v1/traces",
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
   }),
-  instrumentations: [getNodeAutoInstrumentations()]
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+    // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+    // url: '<your-otlp-endpoint>/v1/metrics',
+      headers: {}, // an optional object containing custom headers to be sent with each request
+      concurrencyLimit: 1, // an optional limit on pending requests
+    }),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
 });
-
-sdk.start()
+sdk.start();
