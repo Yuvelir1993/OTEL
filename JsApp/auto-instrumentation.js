8b13eb1988657b1a2169e3fafedb7711a8e3d56f
plozovik <plozovikov@gmail.com>
2023-04-30 20:00:32 +0200

    JsApp: Export to OTEL colelctor -> Jaeger fucking works

--- JsApp/auto-instrumentation.js
+++ JsApp/auto-instrumentation.js
@@ -1,24 +1,29 @@
-/* Auto isntrumentation */
 const opentelemetry = require("@opentelemetry/sdk-node");
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 const {
   getNodeAutoInstrumentations,
 } = require("@opentelemetry/auto-instrumentations-node");
 const {
-  OTLPTraceExporter,
+  OTLPTraceExporter
 } = require("@opentelemetry/exporter-trace-otlp-proto");
 const {
   OTLPMetricExporter
 } = require("@opentelemetry/exporter-metrics-otlp-proto");
-const {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter
-} = require('@opentelemetry/sdk-metrics');
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: "http://localhost:4318/v1/traces",
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+  }),
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter()
+    exporter: new OTLPMetricExporter({
+      url: "http://localhost:4318/v1/metrics",
+      // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+      headers: {}, // an optional object containing custom headers to be sent with each request
+      concurrencyLimit: 1, // an optional limit on pending requests
+    }),
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
