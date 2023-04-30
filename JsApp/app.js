3e929f6c789641bc5875342f9f113c4e6bfbff86
plozovik <plozovikov@gmail.com>
2023-04-30 21:06:54 +0200

    JsApp: Called python apps and returneed response

--- JsApp/app.js
+++ JsApp/app.js
@@ -6,16 +6,20 @@ function generateRandNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-app.get("/entry", (req, res) => {
-  fetch("http://localhost:7080/prime")
+app.get("/entry", async (req, res) => {
+  const response = await fetch("http://127.0.0.1:7080/prime")
     .then((response) => {
       // Do something with response
       console.log("DID IT!");
+      console.log(response);
     })
     .catch(function (err) {
       console.log("Unable to fetch -", err);
+      return err;
     });
-  return (generateRandNumber(1, 6).toString());
+  console.log("RESPONSE after prime call");
+  console.log(response);
+  res.send(generateRandNumber(1, 6).toString());
 });
 
 app.listen(PORT, () => {
