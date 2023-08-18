c1a47086677c4f0344c4b3f4e7a87b327852a7e6
plozovik <plozovikov@gmail.com>
2023-08-18 18:07:24 +0200

    Article. Cleaning up unneeded things.

--- JsPrimeApp/app.js
+++ JsPrimeApp/app.js
@@ -1,75 +1,33 @@
-const { W3CTraceContextPropagator, W3CBaggagePropagator } = require("@opentelemetry/core");
-const {
-    defaultTextMapSetter,
-    ROOT_CONTEXT,
-} = require("@opentelemetry/api");
+const { W3CTraceContextPropagator } = require("@opentelemetry/core")
 const autoinstrExpressModule = require('./auto-instrumentation-express')
-const { trace, context, opentelemetry } = require("@opentelemetry/api");
+const { context, defaultTextMapSetter, opentelemetry } = require("@opentelemetry/api");
 const tracer = autoinstrExpressModule.setupTracing('JsPrimeApp');
 
 const express = require("express");
 const PORT = parseInt(process.env.PORT || "8080");
 const app = express();
 
-function generateRandNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-app.post("/commonContext", async (req, res) => {
-    await fetch("http://127.0.0.1:8090/commonContext")
-        .then((response) => {
-            res.send("Called 'commonContext' API.");
-        })
-        .catch(function (ex) {
-            console.log("Unable to fetch -", ex);
-            res.status(500);
-            res.send(ex);
-        });
-})
-
 app.get("/jsPrime", async (req, res) => {
     const callPrimeSpan = tracer.startSpan('jsPrimeEntry');
-    callPrimeSpan.addEvent("REST call start")
     const propagator = new W3CTraceContextPropagator();
-    let carrier = {
-        'my-long-id-carrier': '152156633f7f13adf20053b01198a5b9',
-        'my-short-id-carrier': 'd81a6faefca25c90'
-    };
-
-    callPrimeSpan.setAttribute("email", "Pavel Lozovikov @ mail . com")
-    callPrimeSpan.setAttribute("myEmail", "Pavel Lozovikov @ mail . com")
-    callPrimeSpan.setAttribute("password", "MyPassword123:):):)")
-
+    let carrier = {};
     propagator.inject(
         context.active(),
         carrier,
         defaultTextMapSetter
     );
-    console.log("carrier", carrier); // transport this carrier info to other service via headers or some other way
-
-    // --- context propagation ---
-    // https://github.com/open-telemetry/opentelemetry-js/issues/2458
-    // // In downstream service
-    // const parentCtx = propagator.extract(ROOT_CONTEXT, carrier, defaultTextMapGetter);
-    // const childSpan = tracer.startSpan("child", undefined, parentCtx);
-    // ---------------------------
     const settings = {
         method: 'GET',
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
-            'my-traceparent-id': carrier.traceparent,
-            'my-short-id': 'd81a6faefca25c90'
+            'my-traceparent-id': carrier.traceparent
         }
     };
     tracer.startActiveSpan(callPrimeSpan, async () => {
         await fetch("http://127.0.0.1:8090/jsSecondary", settings)
             .then((response) => {
-                const currentSpanSpanId = callPrimeSpan.spanContext().spanId;
-                console.log(`currentSpanSpanId: ${currentSpanSpanId}`);
-                console.log("DID IT!");
-                console.log(response);
-                res.send(generateRandNumber(1, 6).toString());
+                res.send("Return from the 'jsSecondaryApp'.");
             })
             .catch(function (ex) {
                 callPrimeSpan.recordException(ex);
--- JsSecondaryApp/app.js
+++ JsSecondaryApp/app.js
@@ -1,59 +1,23 @@
 const { W3CTraceContextPropagator } = require("@opentelemetry/core");
-const { context, propagation, trace, metrics } = require('@opentelemetry/api');
-const {
-    defaultTextMapGetter,
-    ROOT_CONTEXT,
-} = require("@opentelemetry/api");
-
+const { context, defaultTextMapGetter } = require('@opentelemetry/api');
 const autoinstrExpressModule = require('./auto-instrumentation-express')
 const tracer = autoinstrExpressModule.setupTracing('JsSecondaryApp');
+const propagator = new W3CTraceContextPropagator();
 
 const express = require("express");
 const PORT = parseInt(process.env.PORT || "8090");
 const app = express();
-const propagator = new W3CTraceContextPropagator();
-
-app.post("/commonContext", async (req, res) => {
-    res.send("Hello from JsSecondaryApp 'commonContext' API.");
-})
 
 app.get("/jsSecondary", async (req, res) => {
-    const myLongId = req.headers['my-traceparent-id'];
-    console.log("myLongId", myLongId);
-    const myShortId = req.headers['my-short-id'];
-    console.log("myShortId", myShortId);
+    const traceparentId = req.headers['my-traceparent-id'];
     let carrier = {
-        traceparent: myLongId
+        traceparent: traceparentId
     };
     const parentCtx = propagator.extract(context.active(), carrier, defaultTextMapGetter);
-    console.log("carrier", carrier);
-
-    // Ingect here as in the JsPrimeApp ?
-    // --- context propagation ---
-    // https://github.com/open-telemetry/opentelemetry-js/issues/2458
-    // In downstream service
-    console.log("parentCtx");
-    console.log(parentCtx);
-    const baggage = propagation.getBaggage(context.active());
-    const baggage2 = propagation.getBaggage(parentCtx);
-    console.log("baggage");
-    console.log(baggage);
-    console.log("baggage2");
-    console.log(baggage2);
-    const currentSpan = trace.getActiveSpan();
-    const currentSpanTraceId = currentSpan.spanContext().traceId;
-    const currentSpanSpanId = currentSpan.spanContext().spanId;
-    console.log(`currentSpanTraceId: ${currentSpanTraceId}`);
-    console.log(`currentSpanSpanId: ${currentSpanSpanId}`);
-    // const jsSecondaryAppSpan = tracer.startSpan("JsSecondaryAppSpan");
-    const jsSecondaryAppSpan = tracer.startSpan("JsSecondaryAppSpan", {
-        kind: 1,
-        attributes: { childSpanKey: 'childSpanValue' },
-    }, parentCtx);
-    const jsSecondaryAppSpanTraceId = jsSecondaryAppSpan.spanContext().traceId;
-    console.log(`jsSecondaryAppSpanTraceId: ${jsSecondaryAppSpanTraceId}`);
-    // ---------------------------
-    res.send("Hello from JsSecondaryApp 'jsSecondary' API.");
+    const jsSecondaryAppSpan = tracer.startSpan("JsSecondaryAppSpan", {}, parentCtx);
+    let msg = `Hello from JsSecondaryApp 'jsSecondary' endpoint. We managed to trace it as part of the trace with id '${traceparentId}' of an upstream endpoint call.`;
+    console.log(msg);
+    res.send(msg);
     jsSecondaryAppSpan.end();
 });
 
